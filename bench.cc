56c0519d1e74c895e80e3f5a7cff12c5ef5c9901
Sagar Mittal <sagar@sagarmittal.com>
2023-02-15 19:32:00 -0800

    Hard code type

--- bench.cc
+++ bench.cc
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <functional>
+#include <map>
 #include <random>
 #include <vector>
 
@@ -9,15 +10,20 @@
 #include "tbb/enumerable_thread_specific.h"
 #include "tbb/parallel_sort.h"
 
-template <typename T>
-std::vector<T> generate(T domain, size_t size) {
+using T = hwy::uint128_t;
+
+const std::vector<T>& generate(uint64_t domain, size_t size) {
+  static std::map<std::pair<uint64_t, size_t>, std::vector<T>> cache;
+  if (auto found = cache.find({domain, size}); found != cache.end()) {
+    return found->second;
+  }
   std::mt19937 rng(0);
-  std::uniform_int_distribution<T> dist(0, domain);
+  std::uniform_int_distribution<uint64_t> dist(0, domain);
   std::vector<T> data(size);
   for (T& datum : data) {
-    datum = dist(rng);
+    datum = T{dist(rng)};
   }
-  return data;
+  return cache[{domain, size}] = std::move(data);
 }
 struct StdSort {
   template <typename T>
@@ -63,10 +69,10 @@ struct StdPartitionHwySort {
   }
 };
 
-template <typename T, typename F>
+template <typename F>
 static void BM_Sort(benchmark::State& state) {
   F sort;
-  const auto data = generate<T>(1 << 20, 1000 << 20);
+  const auto data = generate(10000L << 20, 1000 << 20);
   for (auto _ : state) {
     sort(data);  // Implicit copy.
   }
@@ -74,9 +80,9 @@ static void BM_Sort(benchmark::State& state) {
   state.SetBytesProcessed(state.iterations() * data.size() * sizeof(T));
 }
 
-#define BENCH(impl)                   \
-  BENCHMARK(BM_Sort<int64_t, impl>)   \
-      ->Unit(benchmark::kSecond) \
+#define BENCH(impl)                 \
+  BENCHMARK(BM_Sort<impl>) \
+      ->Unit(benchmark::kSecond)    \
       ->MeasureProcessCPUTime();
 
 BENCH(StdSort);
